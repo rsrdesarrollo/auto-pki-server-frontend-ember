3a378e3600e35616ccfaf15f3546a8b241904bfe
Raúl Sampedro <rsrdesarrollo@gmail.com>
2016-06-30 21:40:37 +0200

    enforce API authorizer token

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
     namespace: 'api/v1',
+    authorizer: 'authorizer:jwt-api',
     updateRecord(store, type, snapshot) {
         var data = {};
         var serializer = store.serializerFor(type.modelName);
@@ -11,7 +13,7 @@ export default DS.JSONAPIAdapter.extend({
         var id = snapshot.id;
         var url = this.buildURL(type.modelName, id, snapshot, 'updateRecord');
 
-        
+
         return this.ajax(url, 'PATCH', { data: data });
     }
 });
--- app/authorizers/jwt-api.js
+++ app/authorizers/jwt-api.js
@@ -2,10 +2,12 @@ import Ember from 'ember';
 import Base from 'ember-simple-auth/authorizers/base';
 
 export default Base.extend({
-  authorize(jqXHR) {
-      var accessToken = this.get('session.content.secure.token');
-      if (this.get('session.isAuthenticated') && !Ember.isEmpty(accessToken)) {
-          jqXHR.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-      }
-  }
+    session: Ember.inject.service(),
+
+    authorize(session, block) {
+        var accessToken = session.token;
+        if (!Ember.isEmpty(accessToken)) {
+            block('Authorization', 'Bearer ' + accessToken);
+        }
+    }
 });
