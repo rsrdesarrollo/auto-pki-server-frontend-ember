b78516e312599bda471993e828b0fcf6eef34bd0
Raúl Sampedro <rsrdesarrollo@gmail.com>
2016-09-11 18:16:46 +0200

    add periodic refresh

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     session: Ember.inject.service(),
+    _setup : function(){
+        // TODO: Use websockets to refresh model only when changes on back-end.
+        
+        let self = this;
+
+        let cb = function(){
+            self.get('target.router').refresh();
+            Ember.run.later(cb, 1000);
+        };
+
+        Ember.run.later(cb, 1000);
+
+    }.on('init'),
 
     actions:{
         logout: function () {
